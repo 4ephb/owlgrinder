a8105120d70f1db131e8986a95979d124e5c183e
Alexander_G <alexander.4ephb@gmail.com>
2024-05-24 01:07:22 +1000

    UPD: index.js

--- index.js
+++ index.js
@@ -1,56 +1,45 @@
-try {
-	process.env.LESSONS = process.env.LESSONS ?? 1;
-
-	const headers = {
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
-		"user-agent":
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-	};
-
-	const { sub } = JSON.parse(
-		Buffer.from(process.env.DUOLINGO_JWT.split(".")[1], "base64").toString(),
-	)
+	try {
+		process.env.LESSONS = process.env.LESSONS ?? 1;
 	
-	const getArgValue = (arg, defaultValue) => {
+		const headers = {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
+			"user-agent":
+				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+		};
+	
+	  const { sub } = JSON.parse(
+		Buffer.from(process.env.DUOLINGO_JWT.split('.')[1], 'base64').toString(),
+	  )
+	
+	  const getArgValue = (arg, defaultValue) => {
 		return arg !== undefined ? parseInt(arg, 10) : defaultValue;
-	};
-
-	const [,, lessonsArg, minIntervalArg, maxIntervalArg] = process.argv;
-
-	const lessons = getArgValue(lessonsArg, 10); // Default value is 10
-	const minInterval = getArgValue(minIntervalArg, 10); // Default value is 10 seconds
-	const maxInterval = getArgValue(maxIntervalArg, 20); // Default value is 20 seconds
-
-	console.log('lesson: %d, minInt: %d, maxInt: %d', lessons, minInterval, maxInterval);
-
-	if (isNaN(lessons) || isNaN(minInterval) || isNaN(maxInterval)) {
+	  };
+	
+	  const [,, lessonsArg, minIntervalArg, maxIntervalArg] = process.argv;
+	
+	  const lessons = getArgValue(lessonsArg, 10); // Default value is 10
+	  const minInterval = getArgValue(minIntervalArg, 10); // Default value is 10 seconds
+	  const maxInterval = getArgValue(maxIntervalArg, 20); // Default value is 20 seconds
+	
+	  console.log('lesson: %d, minInt: %d, maxInt: %d', lessons, minInterval, maxInterval);
+	
+	  if (isNaN(lessons) || isNaN(minInterval) || isNaN(maxInterval)) {
 		console.error('Please provide valid numbers for lessons, minInterval, and maxInterval.');
 		process.exit(1);
-	}
-
-	const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-	const { fromLanguage, learningLanguage, xpGains } = await fetch(
-		`https://www.duolingo.com/2017-06-30/users/${sub}?fields=fromLanguage,learningLanguage,xpGains`,
-		{
-			headers,
-		},
-	).then((response) => response.json());
-
-	console.log(fromLanguage, learningLanguage, sub);
-
-	let xp = 0;
-	let lessonCount = 0;
+	  }
 	
-	const doLesson = async () => {
-        try {
-            if (lessonCount >= process.env.LESSONS) {
-                console.log(`🎉 You won ${xp} XP`);
-                clearInterval(lessonInterval);
-                return;
-            }
-
+	  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+	
+		const { fromLanguage, learningLanguage, xpGains } = await fetch(
+			`https://www.duolingo.com/2017-06-30/users/${sub}?fields=fromLanguage,learningLanguage,xpGains`,
+			{
+				headers,
+			},
+		).then((response) => response.json());
+	
+		let xp = 0;
+		for (let i = 0; i < lessons; i++) {
 			const session = await fetch(
 				"https://www.duolingo.com/2017-06-30/sessions",
 				{
@@ -119,7 +108,7 @@ try {
 						isV2: true,
 						juicy: true,
 						learningLanguage,
-						skillId: xpGains.find(xpGain => xpGain.skillId).skillId, 
+			  skillId: xpGains.find(xpGain => xpGain.skillId).skillId, 
 						smartTipsVersion: 2,
 						type: "SPEAKING_PRACTICE",
 					}),
@@ -127,7 +116,7 @@ try {
 					method: "POST",
 				},
 			).then((response) => response.json());
-
+	
 			const response = await fetch(
 				`https://www.duolingo.com/2017-06-30/sessions/${session.id}`,
 				{
@@ -145,27 +134,19 @@ try {
 					method: "PUT",
 				},
 			).then((response) => response.json());
-
-			xp += response.xpGain;
-			lessonCount += 1;
-
-			const interval = Math.random() * (maxInterval - minInterval) + minInterval;
-			const intervalInMs = interval * 1000;
-			console.log(`{XP: ${response.xpGain}}, Waiting for ${interval.toFixed(2)} seconds before next lesson...`);
-			await sleep(intervalInMs);
-
-		} catch (error) {
-            console.log("❌ Something went wrong");
-            if (error instanceof Error) {
-                console.log(error.message);
-            }
-        }
-    };
-
-	const lessonInterval = setInterval(doLesson, 600); // Run every 0.6 seconds
-} catch (error) {
-	console.log("❌ Something went wrong");
-	if (error instanceof Error) {
-		console.log(error.message);
+	
+		xp += response.xpGain;
+	
+		const interval = Math.random() * (maxInterval - minInterval) + minInterval;
+		const intervalInMs = interval * 1000;
+		console.log(`{XP: ${response.xpGain}}, Waiting for ${interval.toFixed(2)} seconds before next lesson...`);
+		await sleep(intervalInMs);
+		}
+	
+		console.log(`🎉 You won ${xp} XP`);
+	} catch (error) {
+		console.log("❌ Something went wrong");
+		if (error instanceof Error) {
+			console.log(error.message);
+		}
 	}
-}
